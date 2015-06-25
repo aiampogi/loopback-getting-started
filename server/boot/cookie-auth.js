a22338a50afdc333546b2558258436d5790cefee
Aia Patag <aiampogi@gmail.com>
2015-06-25 15:25:09 +0800

    Include initial oAuth2 solution
    also, launch.json is for VS Code.

--- server/boot/cookie-auth.js
+++ server/boot/cookie-auth.js
@@ -6,6 +6,21 @@ loopback = require('loopback');
 
 module.exports = function(app)
 {
+
+var oauth2 = require('loopback-component-oauth2');
+
+  var options = { 
+    dataSource: app.dataSources.db, // Data source for oAuth2 metadata persistence
+    loginPage: '/login', // The login page url
+    loginPath: '/login' // The login form processing url
+  };
+
+  oauth2.oAuth2Provider(
+    app, // The app instance
+    options // The options
+  );
+
+
   //app.use(loopback.cookieParser('secret'));
 
   // access token will be searched in cookies as well.
